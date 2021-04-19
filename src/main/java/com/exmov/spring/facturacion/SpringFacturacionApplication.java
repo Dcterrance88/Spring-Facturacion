42b3119afb323e6a74b219a431a159394fb0c124
Froilan Fernandez <condefroilan@gmail.com>
2021-04-19 10:45:21 -0500

    Iniciando el proyecto apirest facturacion

---
+++ src/main/java/com/exmov/spring/facturacion/SpringFacturacionApplication.java
@@ -0,0 +1,13 @@
+package com.exmov.spring.facturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFacturacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exmov/spring/facturacion/SpringFacturacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exmov.spring.facturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFacturacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
